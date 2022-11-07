f0a9180855703ccd4825d694f63d72e7b28553b7
Hyunsoo Park <smwater130@naver.com>
2022-11-07 18:01:50 +0900

    [Feat] MyObject Anchor 부착 구현

--- Assets/Scripts/MyObject.cs
+++ Assets/Scripts/MyObject.cs
@@ -5,20 +5,43 @@ using UnityEngine.XR.ARFoundation;
 
 public class MyObject : MonoBehaviour
 {
+    public ARAnchor ARAnchor { get; private set; }
     public int Index { get; private set; }
 
+    [SerializeField] private GameObject _buttonUI;
+    private ARAnchorManager _arAnchorManager;
+
+    private void Awake()
+    {
+        _arAnchorManager = GameObject.Find("AR Session Origin").GetComponent<ARAnchorManager>();
+    }
+
+    /// <summary>
+    /// PlaecObject 스크립트에서 오브젝트 각각의 인덱스를 조정하기 위한 메서드
+    /// </summary>
+    /// <param name="value">변경할 인덱스의 값</param>
     public void SetIndex(int value)
     {
         Index = value;
     }
 
+    /// <summary>
+    /// Object의 Button UI를 띄우는 메서드
+    /// </summary>
     public void Click()
     {
-
+        _buttonUI.SetActive(true);
     }
 
-    public void CreateAnchor(ARRaycastHit hit)
+    /// <summary>
+    /// 특정 지점에 로컬 앵커를 부착하는 메서드
+    /// </summary>
+    /// <param name="arHit">ARRaycastHit로 특정된 지점</param>
+    public void CreateAnchor(ARRaycastHit arHit)
     {
-
+        if(arHit.trackable is ARPlane arPlane)
+        {
+            ARAnchor = _arAnchorManager.AttachAnchor(arPlane, arHit.pose);
+        }
     }
 }
