eab8d8458a6fafc002d2a74b3d0d5e77fdd816ac
Hyunsoo Park <smwater130@naver.com>
2022-11-10 10:50:22 +0900

    [Fix] 인덱스 버그 수정

--- Assets/Scripts/Data/AnchorManager.cs
+++ Assets/Scripts/Data/AnchorManager.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 
 public class AnchorManager : SingletonBehaviour<AnchorManager>
 {
-    // 오브젝트에 감지된 다른 오브젝트의 개수를 저장하는 list
-    public List<(int, int)> DetectedObjectCounts { get; private set; }
-
     private bool[] _isInputted;
+    // 오브젝트에 감지된 다른 오브젝트의 개수를 저장하는 list
+    private List<(int, int)> _detectedObjectCounts = new List<(int, int)>();
     private Dictionary<int, List<int>> _connectObjectDictionary = new Dictionary<int, List<int>>();
 
     [SerializeField] private PlaceObject _placeObject;
-
-    private void Start()
-    {
-        DetectedObjectCounts = new List<(int, int)>();
-    }
 
     /// <summary>
     /// 오브젝트 카운팅 배열을 인덱스와 카운터로 나눠 배열에 저장하는 메서드
@@ -25,14 +19,14 @@ public class AnchorManager : SingletonBehaviour<AnchorManager>
     public void SaveDetectedObjectCounts(int[] counts)
     {
         // 기존에 저장된 배열이 있다면 지우고 다시 만듦
-        if (DetectedObjectCounts.Count > 0)
+        if (_detectedObjectCounts.Count > 0)
         {
-            DetectedObjectCounts.Clear();
+            _detectedObjectCounts.Clear();
         }    
 
         for (int i = 0; i < counts.Length; i++)
         {
-            DetectedObjectCounts.Add((i, counts[i]));
+            _detectedObjectCounts.Add((i, counts[i]));
         }
     }
 
@@ -42,54 +36,62 @@ public class AnchorManager : SingletonBehaviour<AnchorManager>
     /// </summary>
     public void SortDetectedObjectCounts()
     {
-        if (DetectedObjectCounts.Count == 0)
+        if (_detectedObjectCounts.Count == 0)
         {
             Debug.Log("정렬할 카운트가 없습니다.");
             return;
         }
 
         // count 기준으로 정렬
-        DetectedObjectCounts.Sort(delegate((int, int) a, (int, int) b)
+        _detectedObjectCounts.Sort(delegate((int, int) a, (int, int) b)
         {
             if (b.Item2 > a.Item2) return 1;
             else if (b.Item2 < a.Item2) return -1;
             else return 0;
         });
 
-        for (int i = 0; i < DetectedObjectCounts.Count; i++)
+        for (int i = 0; i < _detectedObjectCounts.Count; i++)
         {
-            Debug.Log($"index : {DetectedObjectCounts[i].Item1}, count : {DetectedObjectCounts[i].Item2}");
+            Debug.Log($"index : {_detectedObjectCounts[i].Item1}, count : {_detectedObjectCounts[i].Item2}");
         }
     }
 
     public void ConnectObjects()
     {
-        _isInputted = new bool[DetectedObjectCounts.Count];
+        _isInputted = new bool[_detectedObjectCounts.Count];
 
-        for (int i = 0; i < DetectedObjectCounts.Count; i++)
+        if (_connectObjectDictionary.Count != 0)
         {
-            if (_isInputted[i])
+            _connectObjectDictionary.Clear();
+        }
+
+        for (int i = 0; i < _detectedObjectCounts.Count; i++) 
+        {
+            int objectIndex = _detectedObjectCounts[i].Item1;
+
+            if (_isInputted[objectIndex])
             {
-                Debug.Log($"{i}번은 이미 딕셔너리에 넣은 인덱스");
+                Debug.Log($"{objectIndex}번은 이미 딕셔너리에 넣은 인덱스");
+                _connectObjectDictionary.Add(objectIndex, new List<int>());
                 continue;
             }
 
-            _isInputted[i] = true;
+            _isInputted[objectIndex] = true;
 
-            int objectIndex = DetectedObjectCounts[i].Item1;
             List<int> detectedObjects = _placeObject.Objects[objectIndex].GetComponentInChildren<Radar>().DetectedObjectIndexes;
 
             _connectObjectDictionary.Add(objectIndex, detectedObjects);
             
             for (int j = 0; j < detectedObjects.Count; j++)
             {
-                _isInputted[j] = true;
+                int inputtedObjectIndex = detectedObjects[j];
+                _isInputted[inputtedObjectIndex] = true;
             }
         }
 
         for (int i = 0; i < _connectObjectDictionary.Count; i++)
         {
-            Debug.Log($"key : {i} / item : {_connectObjectDictionary[i]}");
+            Debug.Log($"key : {i} / item 개수 : {_connectObjectDictionary[i].Count}");
         }
     }
 }
